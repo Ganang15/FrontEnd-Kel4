554f0e1b397fac4b285d154e45f5b9adf9d85d9d
adamnurk <19104078@ittelkom-pwt.ac.id>
2022-07-01 21:00:15 +0700

    baru

--- src/pages/infoProduct.js
+++ src/pages/infoProduct.js
@@ -1,9 +1,9 @@
 import { NavbarProduct } from "../components/navbar";
 
 export default function infoProduct(){
+
   return (<>
   <NavbarProduct></NavbarProduct>;
-    
-
+  
   </>)
 }
--- src/pages/infoProfile.js
+++ src/pages/infoProfile.js
@@ -3,7 +3,7 @@ import { React, useRef, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { Col, Container, Row, Form, Button, Alert } from "react-bootstrap";
 import axios from "axios";
-import "../css/infoProfile.css";
+// import "../css/infoProfile.css";
 
 export default function infoProfile() {
 	// const navigate = useNavigate();
@@ -49,56 +49,7 @@ export default function infoProfile() {
 	return (
 		<>
 			<NavbarInfo></NavbarInfo>
-			<Container className="d-flex justify-content-center pt-3 my-5 ">
-				<Form className="w-50">
-					<form-group className="">
-						<Form.Label className="upload-button" for="exampleFormControlFile1"></Form.Label>
-						<Form.Control
-							type="file"
-							class="form-control-file"
-							id="exampleFormControlFile1"
-							hidden
-						/>
-					</form-group>
-					<Form.Group className="mb-3" controlId="formBasicText">
-						<Form.Label>Nama*</Form.Label>
-						<Form.Control
-							type="text"
-							className="radius-primary"
-							placeholder="Nama"
-						/>
-					</Form.Group>
-					<Form.Label>Kota*</Form.Label>
-					<Form.Select aria-label="Default select example">
-						<option>Pilih Kota</option>
-						<option value="jakarta">Jakarta</option>
-						<option value="semarang">Semarang</option>
-						<option value="bekasi">Bekasi</option>
-					</Form.Select>
-					<Form.Group className="mb-3" controlId="formBasicText">
-						<Form.Label>Alamat*</Form.Label>
-						<textarea
-							class="form-control"
-							placeholder="Contoh: Jalan Ikan Hiu 33"
-							rows="3"
-						></textarea>
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="formBasicText">
-						<Form.Label>No Handphone*</Form.Label>
-						<Form.Control
-							type="number"
-							className="radius-primary"
-							placeholder="contoh: +628123456789"
-						/>
-					</Form.Group>
-					<Button
-						className="w-100 radius-primary bg-color-secondary"
-						type="submit"
-					>
-						Simpan
-					</Button>
-				</Form>
-			</Container>
+			
 		</>
 	);
 }
