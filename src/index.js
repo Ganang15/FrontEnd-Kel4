beec2b19ddf5ff7076641c0824248b77da200c38
Bimo <bimo1288@gmail.com>
2022-07-14 20:07:46 +0700

    push baru

--- src/index.js
+++ src/index.js
@@ -6,16 +6,30 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import About from "./pages/About";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import InfoProduct from "./pages/InfoProduct";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+ <Provider store={store}>
+   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Register" element={<Register />} />
-      <Route path="/Login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/about/:id" element={<About />} />
+      <Route path="/EditProfil/:id" element={<EditProfil />} />
+      <Route path="/updateproduct/:id" element={<UpdateProduct />} />
+      <Route path="/InfoProduct" element={<InfoProduct />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/seller/daftar-jual" element={<DaftarJual />} />
+      <Route path="/detailProduct/:id" element={<DetailProduct />} />
+      
     </Routes>
   </Router>
+ </Provider>
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,56 +1,125 @@
-import "../css/style.css";
-import React from "react";
-import { Form, Row, Col, Button} from "react-bootstrap";
-import { Link } from "react-router-dom";
-import Regis from "../image/regis.png"
+import { useRef, useState } from "react";
+import { Form, Row, Col, Button, Alert } from "react-bootstrap";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "../css/login.css";
 
-
-const styleLabel = {
-    borderRadius: '10px',
-};
-
-const styleLink = {
-    textDecoration: 'none',
-    color: '#7126B5',
-    fontWeight: 'bold',
-};
 
 export default function Login() {
+
+    const navigate = useNavigate();
+
+    const emailField = useRef("");
+    const passwordField = useRef("");
+
+    const [errorResponse, setErrorResponse] = useState({
+        isError: false,
+        message: "",
+    });
+    console.log(emailField.current.value)
+    console.log(passwordField.current.value) 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onLogin =  (e) => {
+        e.preventDefault();
+        console.log(emailField.current.value)
+        axios.post('http://localhost:2000/api/v1/login', {
+            email:email,
+            password:password
+          })
+          .then((response) => {
+            const result = response.data
+            console.log(response.data);
+            if (result) {
+                localStorage.setItem("token", result.accessToken);
+
+                navigate("/");
+            }
+
+          }, (error) => {
+            console.log(error);
+          });
+        // try {
+        //     const loginRequest =  axios.post(
+        //         "http://localhost:2000/api/v1/login",
+                
+        //         {
+        //             email: email,
+        //             password: password,
+        //         }
+        //     );
+
+        //     const loginResponse = loginRequest;
+        //     console.log(loginResponse);
+
+        //     if (loginResponse.status) {
+        //         localStorage.setItem("token", loginResponse.data.accessToken);
+
+        //         navigate("/");
+        //     }
+        // } catch (err) {
+        //     console.log(err);
+        //     const response = err.response.data;
+
+        //     setErrorResponse({
+        //         isError: true,
+        //         message: response.message,
+        //     });
+        // }
+    };
+    const styleLabel = {
+        borderRadius: '10px',
+    };
+
+    const styleLink = {
+        textDecoration: 'none',
+        color: '#7126B5',
+        fontWeight: 'bold',
+    }
+
+
+
     return (
-        <>
-            <Row>
-                <Col className="register-left ">
-                    <img src={Regis} alt="Regis"/>
-                </Col>
-                <Col className="register-right">
-                    <h3 className="mb-3">Masuk</h3>
-                    <Form>
-                        
-                        <Form.Group className="mb-3">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Contoh: johndee@gmail.com"
-                                style={styleLabel}
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Masukkan Password"
-                                style={styleLabel}
-                            />
-                        </Form.Group>
-                        <Button className="w-100" type="submit" style={styleLabel}>
-                            Daftar
-                        </Button>
-                        <p className="m-4 text-center">
-                            Belum Punya Akun? <Link style={styleLink} to="/register">Daftar di sini</Link>
-                        </p>
-                    </Form>
-                </Col>
-            </Row>
-        </>
+        <Row>
+            <Col className="register-left">
+                <img src="/images/img-register.png" alt=""/>
+            </Col>
+            <Col className="register-right">
+                <h3 className="mb-3">Masuk</h3>
+                <Form onSubmit={onLogin}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            type="text"
+                            ref={emailField}
+                            placeholder="Contoh: johndee@gmail.com"
+                            style={styleLabel}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            ref={passwordField}
+                            placeholder="Masukkan Password"
+                            style={styleLabel}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </Form.Group>
+                    {errorResponse.isError && (
+                        <Alert variant="danger">{errorResponse.message}</Alert>
+                    )}
+                    <Button className="w-100" type="submit" style={styleLabel}>
+                        Masuk
+                    </Button>
+                    <p className="m-4 text-center">
+                        Belum punya akun? <Link style={styleLink} to="/register">Daftar di sini</Link>
+                    </p>
+                </Form>
+            </Col>
+        </Row>
     );
 }
