0d51f6da011f28b2ff411c0757f037899f1018c6
Cendikia <Halizah1612@gmail.com>
2022-07-19 09:22:30 +0700

    update navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import EditProfil from "./pages/EditProfil";
+import EditProfile from "./pages/EditProfile";
 import UpdateProduct from "./pages/UpdateProduct";
 import Account from "./pages/Account";
 import DaftarJual from "./pages/DaftarJual";
@@ -16,25 +16,22 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import InfoProduct from "./pages/InfoProduct";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- <Provider store={store}>
-   <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/about/:id" element={<About />} />
-      <Route path="/EditProfil" element={<EditProfil />} />
-      <Route path="/updateproduct/:id" element={<UpdateProduct />} />
-      <Route path="/InfoProduct" element={<InfoProduct />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/account" element={<Account />} />
-      <Route path="/seller/daftar-jual" element={<DaftarJual />} />
-      <Route path="/detailProduct/:id" element={<DetailProduct />} />
-      
-    </Routes>
-  </Router>
- </Provider>
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about/:id" element={<About />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/updateproduct/:id" element={<UpdateProduct />} />
+        <Route path="/InfoProduct" element={<InfoProduct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/seller/daftar-jual" element={<DaftarJual />} />
+        <Route path="/detailProduct/:id" element={<DetailProduct />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
